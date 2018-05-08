01cda6c641c9c61b38efe8aefd9b2f2e3876ba24
David Dassau <todieahero@hotmail.com>
2018-05-08 18:49:02 -0500

    finished setup

---
+++ DotNetCoreConsole/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNetCoreConsole.Models
+{
+    public class Message
+    {
+        public virtual int Id { get; set; }
+        public virtual string Body { get; set; }
+    }
+}
